26580324a188f7c5078ddd72af6a5e88595f9968
Sadam Salat <sadamsalat@Sadams-MacBook-Air.local>
2025-06-19 23:32:14 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word.lower()
+
+
+    def match(self,wordcompared):
+        sorted_word=sorted(self.word)
+
+        matches=[]
+        for character in wordcompared:
+            if character.lower()!=self.word and sorted(character.lower())==sorted_word:
+                matches.append(character)
+
+        return (matches)
+    
+
+
+sadam=Anagram("listen")
+print(sadam.match(["sadam","adams" "inlets"]))
+
+    
+
+        
